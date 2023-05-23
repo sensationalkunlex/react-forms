e6c9dbdc00a8059c5600a34edcae828860678b6e
Oyekunle.Oyewusi <Oyekunle.Oyewusi@gmail.com>
2023-05-23 19:26:31 +0100

    Cleanup side effect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
-
+import WindowTracker from './components/WindowTracker';
+import React from 'react';
 function App() {
+  const [show, setShow] = React.useState(true)
+    
+  function toggle() {
+      setShow(prevShow => !prevShow)
+  }
+
   return (
 
     <div className="App">
 
       <Form/>
- 
+      <div className="container">
+          <button onClick={toggle}>
+              Toggle WindowTracker
+          </button>
+          {show && <WindowTracker />}
+      </div>
     </div>
   );
+
 }
 
 export default App;
